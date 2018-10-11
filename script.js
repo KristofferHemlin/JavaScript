8c75d31fddfa9e4ffc27e6b70eaa099fd84e0fd2
CLAMON-8244LRQ\Kristoffer Hemlin <kristoffer.hemlin@claremont.se>
2018-10-11 08:55:38 +0200

    Ändrade till Simon

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Blackjack
 // av Kristoffer Hemlin
 //
-console.log("hello, world");
+console.log("Simon");
 //Card variables
 let suits = ['Hjärter', 'Klöver', 'Ruter', 'Spader'];
 let values = ['Ess', 'Kung', 'Drottning', 'Knekt',
